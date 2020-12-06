f93f97960ada344bfd0dd742ef0dc9e217f937f6
俊哥哥 <37032412+IRVING-L@users.noreply.github.com>
2020-12-06 22:22:06 +0800

    第四题

---
+++ 上机实验报告第四题/code_4.py
@@ -0,0 +1,155 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+######################################################################
+def gauss_solve(arr_A,arr_B,arrx,n,width):#高斯消元法，用于求解矩阵M
+    for k in range(n-1):
+        if arr_A[k][k]==0:
+            print('主元为0，消元失败')
+            return -1
+        for i in range(k+1,n):
+            temp=arr_A[i][k] / arr_A[k][k]
+            for j in range(k,n):
+                arr_A[i][j] = arr_A[i][j] - temp * arr_A[k][j]
+            arr_B[i] = arr_B[i] - temp * arr_B[k]
+
+    arrx[n - 1] = arr_B[n - 1] / arr_A[n - 1][n - 1]
+    for k in range(n-1,-1,-1):
+        temp = arr_B[k]
+        for j in range(k+1,n):
+            temp = temp - arr_A[k][j] * arrx[j]
+        arrx[k] = temp / arr_A[k][k]
+###################################
+def spline(x_0,y_0,x):#分段三次样条插值
+    mat_D=np.zeros(len(x_0))
+    for i in range(1,len(x_0)-1):
+        x0_temp=x_0[i-1:i+2]
+        y0_temp=y_0[i-1:i+2]
+        for j in range(1,3):
+            for q in range(2,j-1,-1):
+                y0_temp[q] = (y0_temp[q]-y0_temp[q-1]) / (x0_temp[q]-x0_temp[q-j])
+        mat_D[i]=6*y0_temp[2]
+
+    mat_h=np.zeros(len(x_0))
+    for i in range(1,len(x_0)):
+        mat_h[i]=x_0[i]-x_0[i-1]
+
+    temp=y_0[0:4]
+    for k in range(1,4):
+        for i in range(3,k-1,-1):
+            temp[i] = (temp[i]-temp[i-1]) / (x_0[i]-x_0[i-k])
+    d0=-12*mat_h[1]*temp[3]
+
+    temp=y_0[len(y_0)-4:]
+    for k in range(1,4):
+        for i in range(3,k-1,-1):
+            temp[i] = (temp[i]-temp[i-1]) / (x_0[len(y_0)-4+i]-x_0[len(y_0)-4+i-k])
+    dn=12*mat_h[len(mat_h)-1]*temp[3]
+    mat_D[0]=d0
+    mat_D[len(mat_D)-1]=dn
+
+    mat_A=np.identity(len(x_0))
+    mat_p1=np.zeros(len(x_0))
+    mat_p2=np.zeros(len(x_0))
+    for i in range(len(x_0)-1):
+        mat_p1[i] = mat_h[i + 1] / (mat_h[i] + mat_h[i + 1]) #λi
+        mat_p2[i] = 1 - mat_p1[i] #μi
+    mat_p1[0] = -2
+    mat_p2[len(x_0)-2] = -2
+    for i in range(len(x_0)):
+        mat_A[i][i] = 2
+        if i-1>=0:
+            mat_A[i][i - 1] = mat_p2[i - 1]
+        if i+1<len(x_0):
+            mat_A[i][i + 1] = mat_p1[i]
+
+    mat_M=np.zeros(len(x_0))
+    gauss_solve(mat_A, mat_D, mat_M, len(x_0), 1)
+
+    y=np.zeros(len(x))
+    for i in range(len(x)):
+        k=0
+        for j in range(1,len(x_0)):
+            if x[i]<=x_0[j]:
+                k=j
+                break
+        if k>0:
+            a1 = (x_0[k] - x[i])
+            a2 = (x[i] - x_0[k - 1])
+            h = mat_h[k]
+            y[i] = ( mat_M[k - 1] * a1 * a1 * a1 / 6
+			+ mat_M[k] * a2 * a2 * a2 / 6
+			+ (y_0[k - 1] - mat_M[k - 1] * h * h / 6) * a1
+			+ (y_0[k] - mat_M[k] * h * h / 6) * a2 ) / h
+            #y[i]=-y[i]
+        else:
+            print("数据超出范围")
+            return -1
+    return y
+###################################
+def TR(sy,a,b,error):
+    #1
+    h1=b-a
+    #2
+    Tn=h1*(sy[0]+sy[-1])/2
+    #3
+    flag0=1
+    while flag0:
+        h2=h1/2 
+        S=0
+        x=a+h2
+        #4
+        flag1=1
+        while flag1:
+            if x>b:
+                flag1=0
+            else:
+                S+=sy[int((x-a)/(b-a)*len(sy))]
+                x+=h1
+        T2n=(Tn+h1*S)/2
+        if abs(T2n-Tn)<error:
+            I=T2n
+            flag0=0 
+        else:
+            Tn=T2n
+            h1=h2
+    return I
+###################################
+def Romberg(sy,a,b,error,k=10):
+    h1=b-a
+    T=np.zeros(k)
+    T[0]=(sy[0]+sy[-1])*h1/2
+    for i in range(1,5):
+        h2=h1/2
+        S=0
+        x=a+h2
+        while 1:
+            if x<b:
+                S+=sy[int((x-a)/(b-a)*len(sy))]
+                x+=h1
+            else:
+                break
+        T[i]=(T[i-1]+h1*S)/2
+        h1=h2
+    print(T)
+    S=np.zeros(k)
+    for i in range(4):
+        S[i]=T[i+1]+(T[i+1]-T[i])/3 
+    print(S)
+    C=np.zeros(k)
+    for i in range(3):
+        C[i]=S[i+1]+(S[i+1]-S[i])/15
+    print(C)
+    R=np.zeros(2)
+    for i in range(2):
+        R[i]=C[i+1]+(C[i+1]-C[i])/63
+    print(R)
+######################################################################
+x0=np.linspace(0,1,11)
+y0=[]
+for i in range(11):
+    y0.append(np.exp(x0[i]))
+beg=time.time()
+x=np.linspace(0,1,10000)
+s_y=spline(x0,y0,x)
+Romberg(s_y,0,1,0.00001)
